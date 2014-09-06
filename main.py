ee36ef41e0507556abd4928c094519558122285c
bob1983 <bob.1983.g@gmail.com>
2014-09-07 01:03:21 +0900

    WIP

--- main.py
+++ main.py
@@ -64,10 +64,49 @@ def cleanup_source_code(src_code_text):
     src_code_text = remove_blank(src_code_text)
     return src_code_text
 
+def check_line_type(text):
+    if is_package(text):
+        return "package"
+    elif is_class(text):
+        return "class"
+    elif is_method(text):
+        return "method"
+    elif has_left_brace(text):
+        return "control flow"
+    elif has_right_brace(text):
+        return "finish"
+    return "line"
+
+def is_package(text):
+    package_reg_str = 'package [a-zA-Z]*{\n'
+    package_reg     = re.compile(package_reg_str, re.MULTILINE)
+    return re.match(package_reg, text)
+
+def is_class(text):
+    class_reg_str   = 'class [a-zA-Z]*{\n'
+    class_reg       = re.compile(class_reg_str, re.MULTILINE)
+    return re.match(class_reg, text)
+
+def is_method(text):
+    method_reg_str  = 'function [a-zA-Z]*{\n'
+    method_reg      = re.compile(method_reg_str, re.MULTILINE)
+    return re.match(method_reg, text)
 
 if __name__ == '__main__':
     filename = sys.argv[1]
     parser = ASParser(filename)
-    
+
+    element_stack = []
+    for 
+        line_text = parser.get_available_line()
+        line_type = check_line_type(line_text)
+        if line_type == "package" || line_type == "class" || line_type == "method" || line_type == "control flow":
+            element = Element.new(line_type)
+            element_stack.push(element)
+        else if line_type == "finish":
+            element_stack.pop
+        current_element = element_stack.last_object
+        current_element.addOneStep()
+
     parser.print_src_code()
     #parser.print_org_src_code()
