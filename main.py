5812936cc870e7b3dcd6107486e5ed1e35859252
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2014-09-07 00:14:47 +0900

    Implemented template main class, ASParser. Just now, this is only cleanup (remove comment and blank) source code.

--- main.py
+++ main.py
@@ -3,27 +3,42 @@
 import sys
 import re
 
-def remove_comment(text):
+class ASParser(object):
+    """docstring for ASParser"""
+    def __init__(self, src_code_file_name):
+        super(ASParser, self).__init__()
+        file = open(src_code_file_name, 'r')
+        self.org_src_code = file.read()
+        self.src_code = cleanup_source_code(self.org_src_code)
+
+    def print_src_code(self):
+        print self.src_code
+
+    def print_org_src_code(self):
+        print self.org_src_code
+
+def remove_comment(src_code_text):
     comment_reg_str = '//.*\n'
     comment_reg = re.compile(comment_reg_str, re.MULTILINE)
-    text = re.sub(comment_reg, '\n', text)
-    return text
+    src_code_text = re.sub(comment_reg, '\n', src_code_text)
+    return src_code_text
 
-def remove_blank(text):
+
+def remove_blank(src_code_text):
     blank_reg_str = '^\s*\n'
     blank_reg = re.compile(blank_reg_str, re.MULTILINE)
-    text = re.sub(blank_reg, '', text)
-    return text
+    src_code_text = re.sub(blank_reg, '', src_code_text)
+    return src_code_text
 
-def cleanup_source_text(text):
-    text = remove_comment(text)
-    text = remove_blank(text)
-    return text
+
+def cleanup_source_code(src_code_text):
+    src_code_text = remove_comment(src_code_text)
+    src_code_text = remove_blank(src_code_text)
+    return src_code_text
+
 
 if __name__ == '__main__':
     filename = sys.argv[1]
-    file = open('sample.as', 'r')
-    text = file.read()
-    text = cleanup_source_text(text)
-    print text
-
+    parser = ASParser(filename)
+    parser.print_src_code()
+    #parser.print_org_src_code()
