55b845fcd08e685e8cbe289f99933f7a17ffe35c
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2014-09-06 23:50:33 +0900

    Implemented remove_blank function.

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 
 import sys
+import re
 
-def test1():
-    print "a"
+def remove_blank(text):
+    res = re.sub(re.compile('^\s*\n', re.MULTILINE), '', text)
+    return res
 
 if __name__ == '__main__':
     filename = sys.argv[1]
-    file = open(filename, 'r')
-    for l in file.readline():
-        print l
-
-
+    file = open('sample.as', 'r')
+    text = file.read()
+    text = remove_blank(text)
+    print text
 
